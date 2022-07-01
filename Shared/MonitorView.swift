2451ce9ae908c5f8c51a4e5b991b0cad527245a7
Bob Jacobsen <jacobsen@mac.com>
2022-07-01 17:44:15 -0400

    Documentation

--- Shared/MonitorView.swift
+++ Shared/MonitorView.swift
@@ -11,13 +11,14 @@ import OpenlcbLibrary
 struct MonitorView: View {  // TODO: Add useful stuff from the monitor stream
     @ObservedObject var monitorModel:MonitorModel = MonitorModel.sharedInstance
     
+    // TODO: Add some nice scrolling control so it stays at the bottom until user wants to stick on something
     var body: some View {
         List {
             ForEach(monitorModel.printingProcessorContentArray, id: \.id) { element in
                 Text(element.line)
             }
         }
-            .navigationTitle("Monitor View")
+        .navigationTitle("Monitor View")
     }
 }
 
