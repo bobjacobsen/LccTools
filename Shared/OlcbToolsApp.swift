4021039979dee82aed914c74a9e4d13130697451
Bob Jacobsen <jacobsen@mac.com>
2022-07-01 13:01:27 -0400

    Configure for receive from hub complete; starts up when run

--- Shared/OlcbToolsApp.swift
+++ Shared/OlcbToolsApp.swift
@@ -7,24 +7,45 @@
 
 import SwiftUI
 import OpenlcbLibrary
+import TelnetListenerLib
+import Network
 import os
 
 @main
 struct OlcbToolsApp: App {
-    @AppStorage("THIS_NODE_ID") private var this_node_ID: String = "05.01.01.01.03.FF"
+    @AppStorage("HUB_IP_ADDRESS") private var ip_address: String = "localhost"          // see ``SettingsView``
+    @AppStorage("THIS_NODE_ID") private var this_node_ID: String = "05.01.01.01.03.FF"  // see ``SettingsView``
+
+    static let openlcblib = OpenlcbLibrary(defaultNodeID: NodeID("05.01.01.01.03.FF")) // TODO: using this_node_ID results in "Cannot use instance member 'this_node_ID' within property initializer; property initializers run before 'self' is available"
     
-    static let openlcblib = OpenlcbLibrary(defaultNodeID: NodeID("05.01.01.01.03.FF")) // using this_node_ID results in "Cannot use instance member 'this_node_ID' within property initializer; property initializers run before 'self' is available"
+    let telnetclient : TelnetClient
     
-    let canphysical = CanPhysicalLayerSimulation() //  TODO: figure out how to make this a real (not simulated) connection even while testing
+    let canphysical : CanPhysicalLayer
+    // TODO: figure out how to make this a real (not simulated) connection even while testing
+    
+    let logger = Logger(subsystem: "org.ardenwood.OlcbLibDemo", category: "OlcbToolsApp")
     
     init () {
+ 
+        // create, but not yet connect, the Telnet connection to the hub
+        telnetclient = TelnetClient(host: "192.168.1.206", port: 12021) // TODO: connection to AppStorage ip_address
 
+        // initialize the OLCB processor
+        canphysical = CanPhysicalLayerGridConnect(callback: telnetclient.sendString)
         OlcbToolsApp.openlcblib.configureCanTelnet(canphysical)
         OlcbToolsApp.openlcblib.createSampleData()
         
-        print ("at startup: \($this_node_ID) \(this_node_ID)")
+        // log some info
+        let temp_this_node_ID = self.this_node_ID   // avoid "capture of mutating self" compile error
+        logger.info("at startup, this program's default node ID is set to: \(temp_this_node_ID)")
+        let temp_hub_ip_address = self.ip_address   // avoid "capture of mutating self" compile error
+        logger.info("at startup, default hub IP address is set to: \(temp_hub_ip_address)")
 
-        //let logger = Logger(subsystem: "org.ardenwood.OlcbLibDemo", category: "OlcbToolsApp")
+        // start the connection
+        telnetclient.start()
+        
+        // start the OLCB layer
+        canphysical.physicalLayerUp()
      }
     
     let persistenceController = PersistenceController.shared
@@ -34,6 +55,7 @@ struct OlcbToolsApp: App {
         // macOS puts those in a tab bar at the top of the window
         WindowGroup {
             TabView {
+//                // We're no longer using the default ContentView name for the base view
 //                ContentView()
 //                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
 //                    .tabItem {
