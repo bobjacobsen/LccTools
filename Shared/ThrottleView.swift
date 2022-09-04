67349f7199eeda61ae8e0e1f4ea364455b9de146
Bob Jacobsen <jacobsen@mac.com>
2022-09-04 08:05:36 +0200

    Update TODO

--- Shared/ThrottleView.swift
+++ Shared/ThrottleView.swift
@@ -215,6 +215,7 @@ struct LocoSelectionView : View {
     let logger = Logger(subsystem: "us.ardenwood.OlcbTools", category: "LocoSelectionView")
 
     var body: some View {
+        // TODO: add a search component above the roster that narrows down the roster selection?
         VStack {
             Text("Select Locomotive")
                 .font(.largeTitle)
