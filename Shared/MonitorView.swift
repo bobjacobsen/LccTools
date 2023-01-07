990376231b3f437539d13b2aa96084a26f0e5b79
Bob Jacobsen <jacobsen@mac.com>
2023-01-07 15:16:48 -0500

    Add start and end buttons to monitor

--- Shared/MonitorView.swift
+++ Shared/MonitorView.swift
@@ -7,33 +7,69 @@
 import SwiftUI
 import OpenlcbLibrary
 
+// Internal helper class for moving to top/bottom
+class ScrollToModel: ObservableObject {
+    enum Action {
+        case end
+        case top
+    }
+    @Published var direction: Action? = nil
+}
+
 /// Displayes the contents from the PrintingProcessor, e.g. the OpenLCB traffic monitor
 struct MonitorView: View {
+    @StateObject var vm = ScrollToModel()
 
     // single global observed object contains monitor info
     @ObservedObject var monitorModel:MonitorModel = MonitorModel.sharedInstance
     
     // TODO: Add some nice scrolling control so it stays at the bottom until user wants to stick on something
     var body: some View {
-        VStack() {
-            ScrollView {
-                VStack(spacing: 3) {
-                    ForEach(monitorModel.printingProcessorContentArray, id: \.id) { element in
-                        HStack {
-                            Text(element.line)
-                                .font(.callout)
-                            Spacer()  // force alignment of text to left
+        VStack {
+            ScrollViewReader { sp in
+               ScrollView {
+                    LazyVStack(spacing: 3) {
+                        ForEach(monitorModel.printingProcessorContentArray, id: \.id) { element in
+                            HStack {
+                                Text(element.line)
+                                    .font(.callout)
+                                Spacer()  // force alignment of text to left
+                            }
+                            Divider()
                         }
-                        Divider()
+                    }.onReceive(vm.$direction) { action in
+                        guard !monitorModel.printingProcessorContentArray.isEmpty else { return }
+                        withAnimation {
+                            switch action {
+                            case .top:
+                                sp.scrollTo(monitorModel.printingProcessorContentArray.first!.id, anchor: .top)
+                            case .end:
+                                sp.scrollTo(monitorModel.printingProcessorContentArray.last!.id, anchor: .bottom)
+                            default:
+                                return
+                            }
+                        }
                     }
+                    .navigationTitle("Monitor View")
                 }
-                .navigationTitle("Monitor View")
             }
             Spacer()
-            StandardClickButton(label:"Clear",
-                                height: SMALL_BUTTON_HEIGHT,
-                                font: SMALL_BUTTON_FONT){
-                monitorModel.printingProcessorContentArray = []
+            HStack {
+                StandardClickButton(label:"Start",
+                                    height: SMALL_BUTTON_HEIGHT,
+                                    font: SMALL_BUTTON_FONT){
+                    vm.direction = .top
+                }
+                StandardClickButton(label:"Clear",
+                                    height: SMALL_BUTTON_HEIGHT,
+                                    font: SMALL_BUTTON_FONT){
+                    monitorModel.printingProcessorContentArray = []
+                }
+                StandardClickButton(label:"End",
+                                    height: SMALL_BUTTON_HEIGHT,
+                                    font: SMALL_BUTTON_FONT){
+                    vm.direction = .end
+                }
             }
         }
     }
